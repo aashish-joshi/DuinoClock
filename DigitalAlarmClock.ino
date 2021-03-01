3a65a97117c3b68c8737fa2bb17622d7f9b91efd
Aashish Joshi <aashish@aashishjoshi.co.in>
2021-03-01 08:20:44 +0530

    Add files via upload

--- DigitalAlarmClock.ino
+++ DigitalAlarmClock.ino
@@ -1,6 +1,27 @@
+/*
+ * THE LIBRARIES DS3231.h AND LiquidCrystal_I2C.h
+ * ARE DEVELOPED BY 3RD PARTIES.
+ * 
+ * I DO NOT OWN ANY RIGHTS TO EITHER OF THEM EXCEPT
+ * WHAT IS ALLOWED BY THEIR RESPECTIVE LICENSES.
+ * 
+ * I HAVE INCLUDED THEM IN THE GIT REPO BECAUSE OLD LINKS
+ * SOMETIMES GO MISSING.
+ * 
+ * THE AUTHOR OF DS3231 LIBRARY HAS INCLUDED AN AMAZING
+ * PDF MANUAL. IT IS INCLUDED WITH THE .ZIP
+ * FILE, DO HAVE A LOOK AT IT IF YOU WISH TO MAKE CHANGES.
+ * 
+ * IT DOESN'T LOOK AS IF DS3231 THE LIBRARY IS MAINTAINED
+ * SO ANY POTENTIAL BUGS MAY NOT BE FIXED IN FUTURE.
+ * 
+ * REPORT ISSUES OR REQUIREMENTS AT https://github.com/aashish-joshi/DuinoClock/issues
+ * 
+ */
+
 #include <Wire.h> 
-#include <LiquidCrystal_I2C.h>
-#include <DS3231.h>
+#include <LiquidCrystal_I2C.h>      // SOURCE https://github.com/johnrickman/LiquidCrystal_I2C
+#include <DS3231.h>                 // SOURCE http://www.rinkydinkelectronics.com/library.php?id=73
 
 DS3231 rtc(SDA, SCL);
 
@@ -11,55 +32,82 @@ int temp;
 // buzzer pin number
 const int buzzer = 9;
 
-//vibration motor pin number
+/* Pin number for vibration motor
+ * 
+ * COMMENT THE FOLLOWING LINE OR REMOVE IT
+ * IF YOU DO NOT HAVE A VIBRATION MOTOR
+ */
 const int vibration = 10;
+
 
 void setup()
 {
   Serial.begin(9600);
-  rtc.begin();    // initialize the DS3231
+  rtc.begin();                  // initialize the DS3231
   
-  lcd.init();                      // initialize the lcd 
-  //lcd.init();
-  // TURN THE BACKLIGHT ON
-  lcd.backlight();
+  lcd.init();                   // initialize the LCD 
+  
+  lcd.backlight();              // TURN THE BACKLIGHT ON
 
-  // PRINT STATIC TEXT THAT NEVER CHANGES
+  /*
+   * PRINT STATIC TEXT THAT NEVER CHANGES
+   */
   lcd.setCursor(14,1);
-  lcd.print((char)223); // degree character
+  lcd.print((char)223);       // degree character
   lcd.setCursor(15,1);
-  lcd.print("C"); // print C for celcius
+  lcd.print("C");             // print C for celcius
 
-  pinMode(buzzer, OUTPUT); // Set buzzer - pin 9 as an output
-  pinMode(vibration,OUTPUT); // Set pin 10 as output for vibration motor.
+  pinMode(buzzer, OUTPUT);    // Set buzzer - pin 9 as an output
+  
+  /*
+   * Set pin 10 as output for vibration motor.
+   * 
+   * COMMENT THE FOLLOWING LINE WITH A // OR REMOVE IT
+   * IF YOU DO NOT HAVE A VIBRATION MOTOR
+   */
+  pinMode(vibration,OUTPUT);
 
 }
+
 
 
 void loop()
 {
   // PRINT THE CURRENT DATE
   lcd.setCursor(1,0);
-  lcd.print(rtc.getDateStr(FORMAT_LONG,FORMAT_LITTLEENDIAN,'/'));
 
+  /*
+   * FORMAT_LONG -> print the full year
+   * FORMAT_LITTLEENDIAN -> day-month-year format
+   * 
+   * Interesting article about the little endian date format :-) https://bit.ly/2PkQSJM
+   * 
+   * The date separator used is '/' feel free to change to '-' or something else.
+   * 
+   */
+  lcd.print(rtc.getDateStr(FORMAT_LONG,FORMAT_LITTLEENDIAN,'/'));   // 
+  
   //PRINT THE DAY OF WEEK IN SHORT FORM
   lcd.setCursor(12,0);
   lcd.print(rtc.getDOWStr(FORMAT_SHORT));
 
   // PRINT THE CURRENT TIME IN HH:MM:SS FORMAT
   char *currTime = rtc.getTimeStr();
+  lcd.setCursor(2,1);
+  lcd.print(currTime);
 
   // Sound the buzzer 2 times 
   // if the hour has just changed
   if(currTime[3] == '0' && currTime[4] == '0' && currTime[6] == '0' && currTime[7] == '0'){
 
-    //beep and vibrate
+    // turn the buzzer and vibration
+    // on for 250ms(1/4 second)
     digitalWrite(buzzer,HIGH);
-    digitalWrite(vibration,HIGH);
+    digitalWrite(vibration,HIGH);   // comment this line if you don't have a vibration motor
     delay(250);
-
+    
     //turn off the buzzer for 250ms
-    digitalWrite(buzzer,LOW);   
+    digitalWrite(buzzer,LOW);
     delay(250);
 
     // turn on the buzzer once again for 250ms
@@ -72,17 +120,19 @@ void loop()
     digitalWrite(vibration,LOW);
   }
   else if (currTime[3] == '3' && currTime[4] == '0' && currTime[6] == '0' && currTime[7] == '0'){
+
+    // if it is 30 minutes past the hour
+    // just beep once
     digitalWrite(buzzer,HIGH);
     digitalWrite(vibration,HIGH);
-    delay(250);//wait for 1ms
+    
+    delay(500);                     //wait for 500ms
+    
     digitalWrite(buzzer,LOW);
     digitalWrite(vibration,LOW);
   }
-  
-  lcd.setCursor(2,1);
-  lcd.print(currTime);
 
-  // PRINT THE CURRENT TEMPERATURE
+  // PRINT THE CURRENT TEMPERATURE in C
   lcd.setCursor(12,1);
   temp = rtc.getTemp();
   lcd.print(temp);
